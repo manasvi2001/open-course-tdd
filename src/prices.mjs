fce86e7a10845259c921ab4ccdb037f7a7297680
NC24568_Manasvi <nc24568_manasvi@Ninja-ITs-MacBook-Air.local>
2024-04-05 18:51:41 +0530

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -72,7 +72,7 @@ function createApp(database) {
 
   function calculateReduction(date, date2) {
     let reduction = 0;
-    if (date && date2 && isMonday(date) && !isHoliday(date, date2)) {
+    if (date && isMonday(date) && !isHoliday(date)) {
       reduction = 35;
     }
     return reduction;
