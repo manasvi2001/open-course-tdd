46294c74f8aba04615d7f0eff187d4ea391933ea
NC24568_Manasvi <nc24568_manasvi@Ninja-ITs-MacBook-Air.local>
2024-04-05 18:39:03 +0530

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -54,7 +54,7 @@ function createApp(database) {
   }
 
   function calculateCostForDayTicket(age, date, baseCost, date2) {
-    let reduction = calculateReduction(date2, date2);
+    let reduction = calculateReduction(date, date2);
     if (age === undefined) {
       return Math.ceil(baseCost * (1 - reduction / 100));
     }
