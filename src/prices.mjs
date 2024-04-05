7456703fadd585b8ebc6c337fd84a04920200f84
NC24568_Manasvi <nc24568_manasvi@Ninja-ITs-MacBook-Air.local>
2024-04-05 18:52:35 +0530

    tcr: tests pass

--- src/prices.mjs
+++ src/prices.mjs
@@ -19,9 +19,9 @@ function createApp(database) {
     const age = req.query.age ? parseInt(req.query.age) : undefined;
     const type = req.query.type;
     const baseCost = database.findBasePriceByType(type).cost;
-    const date = parseDate(req.query.date);
+    const date = parseDate(req.query.date, "new");
     const dateNew = parseDate(req.query.date, "new");
-    const cost = calculateCost(age, type, dateNew, baseCost);
+    const cost = calculateCost(age, type, date, baseCost);
     res.json({ cost });
   });
 
